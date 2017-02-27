1b0d9b5961538f93fc8d1a305d94c9ce7086ac5c
Jeysown <jeysownlopez@gmail.com>
2017-02-27 20:05:46 +0800

    add2

--- js/add2.js
+++ js/add2.js
@@ -1,32 +1,30 @@
-$(document).ready(function(){
-  var currentClass = $('.progress-tracker li').attr('class');
-  var previousClass;
-  var thisClass;
 
-  function validate(){
-    if($('#input').val().trim() == ''){
-      thisClass = "skip";
-    }else{
-      thisClass = "done";
-    }
-  }
+   function next(){
+    var thisClass = validate();
+    var currentClass = $('.progress-tracker li').attr('class');
 
-  $('.next').on('click',function () {
-    validate();
+
 
     $('.progress-tracker li.active').removeClass(currentClass).addClass(thisClass).next().addClass('active');
-    $('fieldset.active').removeClass(currentClass).next().addClass('active');
+    $('fieldset.active').removeClass('active').next().addClass('active');
 
-  });
+  }
 
-  $('.previous').on('click',function(){
+  function prev(){
 
-    validate();
-    
-    previousClass  = $('.progress-tracker li.active').prev().attr('class');
+    var thisClass = validate();
+    previousClass  = $('.progress-tracker li').prev().attr('class');
 
     $('.progress-tracker li.active').removeClass('active').addClass(thisClass).prev().removeClass(previousClass).addClass('active');
     $('fieldset.active').removeClass('active').prev().addClass('active');
 
-  });
-});
+  }
+
+
+function validate(){
+  if($('fieldset.active #input').val().trim() == ''){
+    return "skip";
+  }else{
+    return "done";
+  }
+}
